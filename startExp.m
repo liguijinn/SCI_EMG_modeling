051963d0a2c1f6430ad09d786d06418da832609a
liguijinn <146758810+liguijinn@users.noreply.github.com>
2024-12-17 11:38:28 -0500

    Add files via upload

--- startExp.m
+++ startExp.m
@@ -1,4 +1,4 @@
-function startExp(pNum)
+% function startExp(pNum)
 
 %{
     
@@ -22,21 +22,14 @@ Institute of Biomedical Engineering, University of Toronto
 %}
 
 %% Setup
-% pNum = 1;
+
+pNum = 1; % comment out if use as a function
 pCode = ['p',num2str(pNum)];
 rootPath = cd();
 pPath = fullfile(rootPath,pCode);
 
-cd(pPath)
-
-cPath = cd();
-rfolder = dir('MUAP_FBL_*');
-rPath = fullfile(cPath, rfolder.name);
-
-totTrials = 15; % specify number of trials
-
 % repeat the info below to set up "config" to label files/folders
-nMU = 100;
+nMU = 2;
 d = 1;  % skin, 1 mm
 h1 = 3; % fat layer, 3 mm
 cv_mu = 4; % mm/ms
@@ -44,6 +37,18 @@ simT = 30; % ms, simulation time for each SFAP (generated already)
 obsT = 3050; % ms, obervation time
 
 config = [num2str(nMU),'_',num2str(h1*10),'h1_',num2str(cv_mu*10),'cv'];
+
+% set up MUAP_FBL folder, only need to do once for each participant
+sn2mu_fb(pCode,config)
+
+%% 
+cd(pPath)
+
+cPath = cd();
+rfolder = dir('MUAP_FBL_*');
+rPath = fullfile(cPath, rfolder.name);
+
+totTrials = 3; % specify number of trials
 
 %% DF
 outFolder_DF = ['PoolDF_',config,'_',num2str(obsT),'_Tra',num2str(totTrials)];
@@ -57,7 +62,7 @@ cd(op_DF); checkDF = dir(['MUAP_DF_',config,'_tra*']);
 cd(rootPath)
 if length(checkDF) ~= totTrials
 for traNum = 1:totTrials
-    sn3DF_MUPool(config,traNum,rootPath,rPath,op_DF)
+    sn3DF_MUPool(config,traNum,rootPath,rPath,op_DF,nMU)
 end
 end
 
@@ -92,7 +97,7 @@ end
 % one example: varying MF_lev from 10% to 100% with 50% volitional effort,
 % severe UMN damage and mild LMN damage
 
-FBL = 0.1:0.1:1; % varying MF_lev
+FBL = 0.1:0.3:1; % varying MF_lev
 var = 'FBLev'; % for file/folder label
 
 ExcDLev = 0.5; 
@@ -101,8 +106,9 @@ MNLev = 0.8; % mild LMN damage
 LMNidx = 81:100; % remove LMN# 81 - 100 
 % this is only read when LMNrem is empty [] in the sn4PT_MUPool below
 
+cd(rootPath)
 parfor traNum = 1:totTrials
-    cd(rootPath)
+    
     sn4PT_MUPool(var,config,ExcDLev,MNLev,LMNidx,LMNrem,alphaExcD,FBL,...
         simT,obsT,traNum,rPath,op_DF,op_Poten)
 end
